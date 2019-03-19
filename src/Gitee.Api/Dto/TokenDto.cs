869c70d237f79fd8078abb08f3e65caa28ad42c9
MysticBoy <mysticboy@live.com>
2019-03-19 14:44:08 +0800

    填写针对gitee的sdk的 nuge包信息

--- src/Gitee.Api/Dto/TokenDto.cs
+++ src/Gitee.Api/Dto/TokenDto.cs
@@ -9,29 +9,34 @@ namespace Gitee.Api.Dto
     public class TokenDto
     {
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public string access_token { get; set; }
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public string token_type { get; set; }
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public int expires_in { get; set; }
 
         public DateTime Expires => DateTime.Now.AddSeconds(expires_in);
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public string refresh_token { get; set; }
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public string scope { get; set; }
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         public int created_at { get; set; }
     }
--- src/Gitee.Api/SDK.cs
+++ src/Gitee.Api/SDK.cs
@@ -13,8 +13,8 @@ namespace Gitee.Api
 {
     public static class SDK
     {
-        public static string client_id { get;  set; }
-        public static string client_secret { get;  set; }
+        public static string client_id { get; set; }
+        public static string client_secret { get; set; }
 
         public static Task<UserDetail> GetUserAsync(this Session session) => session.Client.GetV5UserAsync(session.Token.access_token);
 
@@ -24,12 +24,13 @@ namespace Gitee.Api
         /// <param name="client"></param>
         /// <see cref="https://gitee.com/api/v5/swagger#/getV5UserRepos"/>
         /// <returns></returns>
-        public static Task<List<Repo>> GetReposAsync(this Session session,int page,int per_page=20)
+        public static Task<List<Repo>> GetReposAsync(this Session session, int page, int per_page = 20)
         {
-            return Request<List<Repo>>(session, "v5/user/repos", new { sort = "full_name", page  , per_page   });
+            return Request<List<Repo>>(session, "v5/user/repos", new { sort = "full_name", page, per_page });
         }
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="session"></param>
         /// <param name="name"></param>
@@ -37,21 +38,22 @@ namespace Gitee.Api
         /// <param name="isPrivate"></param>
         /// <see cref="https://gitee.com/api/v5/swagger#/postV5UserRepos"/>
         /// <returns></returns>
-        public static async Task<Repo> CreateRepoAsync(this Session session, string name, string description,bool isPrivate)
+        public static async Task<Repo> CreateRepoAsync(this Session session, string name, string description, bool isPrivate)
         {
-                return await Request<Repo>(session, "v5/user/repos", new CreateRepo() { access_token= session.Token.access_token, description=description, has_issues=true, has_wiki=true, IsPrivate=isPrivate, name=name } ,Method.POST,true);
+            return await Request<Repo>(session, "v5/user/repos", new CreateRepo() { access_token = session.Token.access_token, description = description, has_issues = true, has_wiki = true, IsPrivate = isPrivate, name = name }, Method.POST, true);
         }
-     /// <summary>
-     /// 此请求方法主要用来弥补自动生成的部分api无法正常使用，使用RestSharp重新实现， 在未来，可能完全取消自动生成部分
-     /// </summary>
-     /// <typeparam name="T"></typeparam>
-     /// <param name="session">会话必要信息</param>
-     /// <param name="resource">请求资源连接字符串</param>
-     /// <param name="parameters">参数类， 根据<paramref name="paramAsJson"/>来决定处理为json还是query条件</param>
-     /// <param name="method">Http方法，默认GET</param>
-     /// <param name="paramAsJson">Gitee大部分api是作为Query传送的，只有少数是 json，因此默认为false</param>
-     /// <returns></returns>
-        public static async Task<T> Request<T>(this Session session, string resource, object parameters, Method method = Method.GET,bool paramAsJson=false) where T : class
+
+        /// <summary>
+        /// 此请求方法主要用来弥补自动生成的部分api无法正常使用，使用RestSharp重新实现， 在未来，可能完全取消自动生成部分
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="session">会话必要信息</param>
+        /// <param name="resource">请求资源连接字符串</param>
+        /// <param name="parameters">参数类， 根据<paramref name="paramAsJson"/>来决定处理为json还是query条件</param>
+        /// <param name="method">Http方法，默认GET</param>
+        /// <param name="paramAsJson">Gitee大部分api是作为Query传送的，只有少数是 json，因此默认为false</param>
+        /// <returns></returns>
+        public static async Task<T> Request<T>(this Session session, string resource, object parameters, Method method = Method.GET, bool paramAsJson = false) where T : class
         {
             T result = default(T);
             try
@@ -73,7 +75,7 @@ namespace Gitee.Api
                     }
                 }
                 var response = await new RestClient(session.BaseURL).ExecuteTaskAsync(request);
-                if ((response.StatusCode == HttpStatusCode.OK || response.StatusCode== HttpStatusCode.Created || response.StatusCode == HttpStatusCode.Accepted)  && !string.IsNullOrEmpty(response.Content))
+                if ((response.StatusCode == HttpStatusCode.OK || response.StatusCode == HttpStatusCode.Created || response.StatusCode == HttpStatusCode.Accepted) && !string.IsNullOrEmpty(response.Content))
                 {
                     var jtk = Newtonsoft.Json.Linq.JToken.Parse(response.Content);
                     result = jtk.ToObject<T>();
@@ -103,14 +105,14 @@ namespace Gitee.Api
         public static async Task<Session> LoginAsync(string username, string password)
         {
             var _session = new Session();
-            _session.Token = await _session.Request<TokenDto>("../oauth/token", new { grant_type = "password", username, password,  client_id, client_secret, scope = "projects user_info issues notes" }, Method.POST);
+            _session.Token = await _session.Request<TokenDto>("../oauth/token", new { grant_type = "password", username, password, client_id, client_secret, scope = "projects user_info issues notes" }, Method.POST);
             _session.Client = new Client(new HttpClient());
             _session.Client.BaseUrl = _session.BaseURL;
             return _session;
-
         }
+
         /// <summary>
-        /// 
+        ///
         /// </summary>
         /// <param name="token"></param>
         /// <see cref="https://gitee.com/oauth/token?grant_type=refresh_token&refresh_token={refresh_token}"/>
@@ -118,11 +120,12 @@ namespace Gitee.Api
         public static async Task<Session> RefreshToken(string refresh_token)
         {
             var _session = new Session();
-            _session.Token = await _session.Request<TokenDto>("../oauth/token", new { grant_type = "refresh_token",  refresh_token}, Method.POST);
+            _session.Token = await _session.Request<TokenDto>("../oauth/token", new { grant_type = "refresh_token", refresh_token }, Method.POST);
             _session.Client = new Client(new HttpClient());
             _session.Client.BaseUrl = _session.BaseURL;
             return _session;
         }
+
         /// <summary>
         /// 根据缓存的 token信息继续使用， 如果没过期的话
         /// </summary>
